2d7114122005acdc9c3dd1bbc3fbc64c7c405f7d
Rahul Panjrath <rahul.panjrath@citrix.com>
2013-12-11 16:48:27 -0800

    Removing copyright.

--- src/trees/treeproblems/LeftViewOfATree.java
+++ src/trees/treeproblems/LeftViewOfATree.java
@@ -1,17 +1,3 @@
-/*
- * Copyright (c) 1998-2013 Citrix Online LLC
- * All Rights Reserved Worldwide.
- *
- * THIS PROGRAM IS CONFIDENTIAL AND PROPRIETARY TO CITRIX ONLINE
- * AND CONSTITUTES A VALUABLE TRADE SECRET.  Any unauthorized use,
- * reproduction, modification, or disclosure of this program is
- * strictly prohibited.  Any use of this program by an authorized
- * licensee is strictly subject to the terms and conditions,
- * including confidentiality obligations, set forth in the applicable
- * License and Co-Branding Agreement between Citrix Online LLC and
- * the licensee.
-*/
-
 package trees.treeproblems;
 
 import graphs.graph.Graph;
