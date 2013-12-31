0168f155d8bb493b3f05d4e893163b5aeb3e5d4d
rpanjrath <rahul.panjrath@yahoo.com>
2013-12-30 16:45:17 -0800

    CircularShiftArray: Adding in-place left shifting.

--- src/arrays/circularshiftintarray/CircularShiftArray.java
+++ src/arrays/circularshiftintarray/CircularShiftArray.java
@@ -18,9 +18,9 @@ public class CircularShiftArray {
             System.out.println("Array must have more than one integer.");
 
         } else {
-            System.out.println("Before shift:--");
+            System.out.println("\nBefore shift:--");
             for (int temp : inputArray) {
-                System.out.println(temp);
+                System.out.print(temp + " ");
             }
             int i = 0;
             int[] outputArray = new int[inputArray.length];
@@ -29,9 +29,9 @@ public class CircularShiftArray {
                 outputArray[k] = inputArray[i];
                 i++;
             }
-            System.out.println("After shift:--");
+            System.out.println("\nAfter shift:--");
             for (int temp : outputArray) {
-                System.out.println(temp);
+                System.out.print(temp + " ");
             }
         }
     }
@@ -41,9 +41,9 @@ public class CircularShiftArray {
             System.out.println("Array must have more than one integer.");
 
         } else {
-            System.out.println("Before shift:--");
+            System.out.println("\nBefore shift:--");
             for (int temp : inputArray) {
-                System.out.println(temp);
+                System.out.print(temp + " ");
             }
             int i = 0;
             shiftSize = shiftSize % inputArray.length;
@@ -56,9 +56,9 @@ public class CircularShiftArray {
                 outputArray[k] = inputArray[i];
                 i++;
             }
-            System.out.println("After shift:--");
+            System.out.println("\nAfter shift:--");
             for (int temp : outputArray) {
-                System.out.println(temp);
+                System.out.print(temp + " ");
             }
         }
     }
@@ -69,23 +69,45 @@ public class CircularShiftArray {
             System.out.println("Array must have more than one integer.");
 
         } else {
-            System.out.println("Before shift:--");
+            System.out.println("\nBefore shift:--");
             for (int temp : inputArray) {
-                System.out.println(temp);
+                System.out.print(temp + " ");
             }
             int start = 0;
             int end = length - (shiftSize % length) - 1;
-            circularShiftToRightInPlace(inputArray, start, end);
-            circularShiftToRightInPlace(inputArray, end + 1, inputArray.length - 1);
-            circularShiftToRightInPlace(inputArray, 0, inputArray.length - 1);
-            System.out.println("After shift:--");
+            swap(inputArray, start, end);
+            swap(inputArray, end + 1, inputArray.length - 1);
+            swap(inputArray, 0, inputArray.length - 1);
+            System.out.println("\nAfter shift:--");
             for (int temp : inputArray) {
-                System.out.println(temp);
+                System.out.print(temp + " ");
             }
         }
     }
 
-    private static void circularShiftToRightInPlace(int[] inputArray, int start, int end) {
+    public static void circularShiftToLeftInPlace(int[] inputArray, int shiftSize) {
+        int length = inputArray.length;
+        if (inputArray.length <= 1) {
+            System.out.println("Array must have more than one integer.");
+
+        } else {
+            System.out.println("\nBefore shift:--");
+            for (int temp : inputArray) {
+                System.out.print(temp + " ");
+            }
+            int start = 0;
+            int end = length - (shiftSize % length) - 1;
+            swap(inputArray, 0, inputArray.length - 1);
+            swap(inputArray, start, end);
+            swap(inputArray, end + 1, inputArray.length - 1);
+            System.out.println("\nAfter shift:--");
+            for (int temp : inputArray) {
+                System.out.print(temp + " ");
+            }
+        }
+    }
+
+    private static void swap(int[] inputArray, int start, int end) {
         int i = start;
         int j = end;
         while (i < j) {
