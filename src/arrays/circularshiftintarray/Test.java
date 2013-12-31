7e89e25a3f515db729618736896894f4f3a4cc7a
rpanjrath <rahul.panjrath@yahoo.com>
2013-12-30 16:45:17 -0800

    CircularShiftArray: Adding in-place left shifting.

--- src/arrays/circularshiftintarray/Test.java
+++ src/arrays/circularshiftintarray/Test.java
@@ -9,7 +9,12 @@ package arrays.circularshiftintarray;
 public class Test {
 
     public static void main(String[] args) {
-        int[] input = new int[]{4, 5, 6, 7};
+        int[] input = new int[]{1, 2, 3, 4, 5, 6};
+
+        CircularShiftArray.circularShiftToRight(input, 13);
         CircularShiftArray.circularShiftToLeft(input, 10);
+
+        CircularShiftArray.circularShiftToRightInPlace(input, 8);
+        CircularShiftArray.circularShiftToLeftInPlace(input, 4);
     }
 }
