26a10a0bc29e2a7dce304804d9b66e0074d45ab3
rpanjrath <rahul.panjrath@yahoo.com>
2014-01-03 16:16:45 -0800

    NestedListSum: Simplifying solution.

--- src/collections/test/TestNestedListSum.java
+++ src/collections/test/TestNestedListSum.java
@@ -15,15 +15,23 @@ public class TestNestedListSum {
 
     public static void main(String[] args) {
         List parentList = new ArrayList();
+        parentList.add(5);
+        parentList.add(4);
+
         List childList1 = new ArrayList();
         childList1.add(2);
         childList1.add(3);
-        parentList.add(2);
+
         List childList11 = new ArrayList();
         childList11.add(1);
+        childList11.add(2);
+
         childList1.add(childList11);
+
         parentList.add(childList1);
-        parentList.add(5);
+
+        parentList.add(8);
+
         System.out.println(NestedListSum.findNestedListSum(parentList));
     }
 }
