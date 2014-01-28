09636983ce6c6a608060e19bab2bd439867219b7
rpanjrath <techpanja@gmail.com>
2014-01-27 16:54:41 -0800

    Isomorphic Strings: Adding pattern matching solution.

--- src/strings/isomorphicstrings/Isomorphic.java
+++ src/strings/isomorphicstrings/Isomorphic.java
@@ -55,4 +55,53 @@ public class Isomorphic {
         return true;
     }
 
+    public static boolean isIsomorphicUsingPattern(String inputString1, String inputString2) {
+        int length1 = inputString1.length();
+        int length2 = inputString2.length();
+        if (length1 != length2) {
+            return false;
+        }
+        if (length1 == 1) {
+            return true;
+        }
+        Map<Character, Integer> characterMap1 = new HashMap<>();
+        Map<Character, Integer> characterMap2 = new HashMap<>();
+        StringBuilder stringBuilder1 = new StringBuilder();
+        StringBuilder stringBuilder2 = new StringBuilder();
+        for (int i = 0; i < length1; i++) {
+            char char1 = inputString1.charAt(i);
+            char char2 = inputString2.charAt(i);
+
+            if (characterMap1.get(char1) == null) {
+                characterMap1.put(char1, i);
+                stringBuilder1.append(i);
+            } else {
+                stringBuilder1.append(char1);
+            }
+
+            if (characterMap2.get(char2) == null) {
+                characterMap2.put(char2, i);
+                stringBuilder2.append(i);
+            } else {
+                stringBuilder2.append(char2);
+            }
+
+            if (!stringBuilder1.toString().equalsIgnoreCase(stringBuilder2.toString())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Isomorphic.isIsomorphic("foo", "bar"));
+        System.out.println(Isomorphic.isIsomorphic("bar", "foo"));
+        System.out.println(Isomorphic.isIsomorphicUsingPattern("foo", "bar"));
+        System.out.println(Isomorphic.isIsomorphicUsingPattern("bar", "foo"));
+        System.out.println(Isomorphic.isIsomorphic("turtle", "tletur"));
+        System.out.println(Isomorphic.isIsomorphic("tletur", "turtle"));
+        System.out.println(Isomorphic.isIsomorphicUsingPattern("turtle", "tletur"));
+        System.out.println(Isomorphic.isIsomorphicUsingPattern("tletur", "turtle"));
+    }
+
 }
