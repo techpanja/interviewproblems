8a858f90673d228b676214705a54e1d41e87410e
rpanjrath <techpanja@gmail.com>
2014-02-03 17:02:20 -0800

    Arrays: Search in rotated sorted array.

--- src/arrays/searchrotatedsortedarray/SearchInRotatedSortedArray.java
+++ src/arrays/searchrotatedsortedarray/SearchInRotatedSortedArray.java
@@ -1,7 +1,7 @@
 package arrays.searchrotatedsortedarray;
 
 /**
- * Search for a number in rotated sorted array. for e.g. 8 9 1 2 3 4 5 6 7
+ * Search for a number in rotated sorted array (ascending or descending). for e.g. 8 9 1 2 3 4 5 6 7
  * Created by techpanja
  * Created on 1/27/14 11:53 AM.
  */
@@ -19,10 +19,11 @@ public class SearchInRotatedSortedArray {
             return inputArray[0] == number;
         }
         int low = 0, high = inputArray.length - 1;
-        return recursiveSearch(inputArray, low, high, number);
+        return recursiveSearchAscendingOrder(inputArray, low, high, number)
+                || recursiveSearchDescendingOrder(inputArray, low, high, number);
     }
 
-    private static boolean recursiveSearch(int[] inputArray, int low, int high, int number) {
+    private static boolean recursiveSearchAscendingOrder(int[] inputArray, int low, int high, int number) {
         if (high < low) {
             return false;
         }
@@ -35,21 +36,52 @@ public class SearchInRotatedSortedArray {
         if (inputArray[low] < inputArray[mid]
                 && inputArray[low] < number
                 && number <= inputArray[mid]) {
-            return recursiveSearch(inputArray, low, mid - 1, number);
+            return recursiveSearchAscendingOrder(inputArray, low, mid - 1, number);
         } else if (inputArray[mid] < inputArray[high]
                 && inputArray[mid] < number
                 && number <= inputArray[high]) {
-            return recursiveSearch(inputArray, mid + 1, high, number);
+            return recursiveSearchAscendingOrder(inputArray, mid + 1, high, number);
         } else if (inputArray[low] > inputArray[mid]) {
-            return recursiveSearch(inputArray, low, mid - 1, number);
+            return recursiveSearchAscendingOrder(inputArray, low, mid - 1, number);
         } else if (inputArray[mid] > inputArray[high]) {
-            return recursiveSearch(inputArray, mid + 1, high, number);
+            return recursiveSearchAscendingOrder(inputArray, mid + 1, high, number);
         } else {
             return false;
         }
     }
 
+    private static boolean recursiveSearchDescendingOrder(int[] inputArray, int low, int high, int number) {
+        if (high < low) {
+            return false;
+        }
+        int mid = (low + high) / 2;
+        if (inputArray[low] == number
+                || inputArray[mid] == number
+                || inputArray[high] == number) {
+            return true;
+        }
+        if (inputArray[low] > inputArray[mid]
+                && inputArray[low] > number
+                && number >= inputArray[mid]) {
+            return recursiveSearchDescendingOrder(inputArray, low, mid - 1, number);
+        } else if (inputArray[mid] > inputArray[high]
+                && inputArray[mid] > number
+                && number >= inputArray[high]) {
+            return recursiveSearchDescendingOrder(inputArray, mid + 1, high, number);
+        } else if (inputArray[low] < inputArray[mid]) {
+            return recursiveSearchDescendingOrder(inputArray, low, mid - 1, number);
+        } else if (inputArray[mid] < inputArray[high]) {
+            return recursiveSearchDescendingOrder(inputArray, mid + 1, high, number);
+        } else {
+            return false;
+        }
+    }
+
+
     public static void main(String[] args) {
         System.out.println(searchInRotatedSortedArray(new int[]{8, 9, 10, 1, 2, 3, 4, 5, 6, 7}, 24));
+        System.out.println(searchInRotatedSortedArray(new int[]{3, 2, 1, 6, 5, 4}, 41));
+        System.out.println(searchInRotatedSortedArray(new int[]{3, 2, 1, 6, 5, 4}, 5));
+        System.out.println(searchInRotatedSortedArray(new int[]{4, 5, 6, 1, 2, 3, 4}, 6));
     }
 }
