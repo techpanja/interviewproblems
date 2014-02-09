b18445e6385750534c23395648c532158e1676d2
rpanjrath <techpanja@gmail.com>
2014-02-08 17:12:04 -0800

    Array: Count number of paths in M * N matrix.

---
+++ src/arrays/allpathsinamatrix/AllPathsInAMatrix.java
@@ -0,0 +1,65 @@
+package arrays.allpathsinamatrix;
+
+import numberproblems.factorial.Factorial;
+
+/**
+ * Given M * N matrix find count of all distinct paths from top left cell to bottom right. You can only move
+ * downwards and to the right from a given cell.
+ * Created by techpanja
+ * Created on 2/7/14 1:52 PM.
+ */
+public class AllPathsInAMatrix {
+
+    private AllPathsInAMatrix() {
+
+    }
+
+    /*
+    * Count of all paths from top left to bottom right. Can move one position left or right from a given cell.
+    * */
+    public static int countOfAllPaths(int m, int n) {
+        if (m == 0 || n == 0) {
+            return 1;
+        }
+        return countOfAllPaths(m - 1, n) + countOfAllPaths(m, n - 1);
+        // if diagonal moment allowed just add + countOfAllPaths(m - 1, n - 1)
+    }
+
+    public static int numberOfPaths(int m, int n) {
+        // Create a 2D table to store results of subproblems
+        int count[][] = new int[m][n];
+
+        // Count of paths to reach any cell in first column is 1
+        for (int i = 0; i < m; i++)
+            count[i][0] = 1;
+
+        // Count of paths to reach any cell in first column is 1
+        for (int j = 0; j < n; j++)
+            count[0][j] = 1;
+
+        // Calculate count of paths for other cells in bottom-up manner using
+        // the recursive solution
+        for (int i = 1; i < m; i++)
+        {
+            for (int j = 1; j < n; j++)
+
+                // By uncommenting the last part the code calculatest he total
+                // possible paths if the diagonal Movements are allowed
+                count[i][j] = count[i-1][j] + count[i][j-1]; //+ count[i-1][j-1];
+
+        }
+        return count[m-1][n-1];
+    }
+
+    public static long countOfAllPathsFormula(int m, int n) {
+        return (Factorial.findFactorial(m + n)) / (Factorial.findFactorial(m) * Factorial.findFactorial(n));
+    }
+
+    public static void main(String[] args) {
+        int numberOfRows = 3;
+        int numberOfCols = 3;
+        System.out.println(countOfAllPaths(numberOfRows - 1, numberOfCols - 1));
+        System.out.println(numberOfPaths(numberOfRows, numberOfCols));
+        System.out.println(countOfAllPathsFormula(numberOfRows - 1, numberOfCols - 1));
+    }
+}
